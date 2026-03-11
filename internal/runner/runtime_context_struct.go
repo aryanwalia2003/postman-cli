3b288d6d31368d5e721de2f7a4eb2d3810db84ae
aryan-otomashen <aryan.int@otomashen.com>
2026-03-11 19:25:01 +0530

    socket io laga diya

--- internal/runner/runtime_context_struct.go
+++ internal/runner/runtime_context_struct.go
@@ -7,3 +7,18 @@ type RuntimeContext struct {
 	GlobalVariables map[string]interface{}//this is a map that will hold the global variables
 	Environment     *environment.Environment //this is a pointer to the environment struct that will hold the environment variables
 }
+
+//this struct looks like 
+// {
+// 	"globalVariables": {
+// 		"key": "value"
+// 	},
+// 	"environment": {
+// 		"name": "dev",
+// 		"variables": {
+// 			"key": "value"
+// 		}
+// 	}
+// }
+
+//this struct is used 
--- internal/scripting/script_runner_iface.go
+++ internal/scripting/script_runner_iface.go
@@ -7,6 +7,6 @@ import (
 
 // ScriptRunner defines the interface for executing pre-request and test scripts.
 type ScriptRunner interface {
-	Execute(script *collection.Script, env *environment.Environment) error
+	Execute(script *collection.Script, env *environment.Environment) error //takes a pointer to the script struct and a pointer to the environment struct and returns an error
 }
 
